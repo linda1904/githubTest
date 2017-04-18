ee5bbcf5fb9c14f4fdb6f320ace32f559f75756c
PC219-10\NTUT <linda1904linda1904@gmail.com>
2017-04-18 10:39:35 +0800

    test_4

--- src/test/githubTest.java
+++ src/test/githubTest.java
@@ -13,8 +13,6 @@ public class githubTest {
 		System.out.println("==============================================");
 		
 		System.out.println("1.HEllo, Java World!");
-		System.out.println("2.Hello, Java World!");
-		System.out.println("3.Hello, Java World!");
 		
 	}
 
