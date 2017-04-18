f9c6c385a22c1cc232e31176c44916556b813575
PC219-10\NTUT <linda1904linda1904@gmail.com>
2017-04-18 10:45:22 +0800

    no message

--- src/test/githubTest.java
+++ src/test/githubTest.java
@@ -12,9 +12,7 @@ public class githubTest {
 		System.out.println("==============================================");
 		System.out.println("==============================================");
 		
-		System.out.println("1.HEllo, Java World!");
-		System.out.println("2.Hello, Java World!");
-		System.out.println("3.Hello, Java World!");
+		
 		
 	}
 
